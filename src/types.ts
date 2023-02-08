fee6d6d8e9beb3fe00b142486dd0297cf9e0da41
AqingCyan <aqingcyan@gmail.com>
2023-02-09 01:22:13 +0800

    feat: add notes about type file

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,13 @@
 export interface EnvVirtualModulePluginOptions {
+  /** Parameters of the vite directive */
   mode: string
+
+  /** plugin resolveId */
   resolveId: string
-  combineData: any
+
+  /** you can merge data into the virtual module */
+  combineData?: Record<string, any>
+
+  /** the prefix of the name of the data allowed to be fetched in the env file */
   envConstantPrefix: string | string[]
 }
